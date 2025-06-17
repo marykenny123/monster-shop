96a5dcd2c597b442dd28d9503d432c95e975f830
mary kenny <marykenny2704@yahoo.com>
2025-06-17 12:22:22 +0200

    correct application.properties error

--- src/main/java/com/femcoders/monster_shop/models/Review.java
+++ src/main/java/com/femcoders/monster_shop/models/Review.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "reviews")
 public class Review {
-    @id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
---
+++ src/main/java/com/femcoders/monster_shop/services/ProductService.java
@@ -0,0 +1,21 @@
+package com.femcoders.monster_shop.services;
+
+import com.femcoders.monster_shop.models.Product;
+import com.femcoders.monster_shop.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+}
