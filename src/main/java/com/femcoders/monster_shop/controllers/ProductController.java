8bd36c0b61abba519773c538d907e12891832ca6
mary kenny <marykenny2704@yahoo.com>
2025-06-18 12:50:53 +0200

    feat(productcontroller,productservice):add getbyid and update methods)

--- src/main/java/com/femcoders/monster_shop/controllers/ProductController.java
+++ src/main/java/com/femcoders/monster_shop/controllers/ProductController.java
@@ -2,11 +2,13 @@ package com.femcoders.monster_shop.controllers;
 
 import com.femcoders.monster_shop.models.Product;
 import com.femcoders.monster_shop.services.ProductService;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
@@ -16,16 +18,35 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @PostMapping("/api/products")
+    public ResponseEntity<Product> addProduct(@RequestBody Product newProduct) {
+        Product createdProduct = productService.addProduct(newProduct);
+        return new ResponseEntity<Product>(createdProduct, HttpStatus.CREATED);
+    }
+
     @GetMapping("/api/products")
     public ResponseEntity<List<Product>> getAllProducts() {
         List<Product> products = productService.getAllProducts();
         return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
     }
 
-    @PostMapping("/api/products")
-    public ResponseEntity<Product> addProduct(@RequestBody Product newProduct) {
-        Product createdProduct = productService.addProduct(newProduct);
-        return new ResponseEntity<Product>(createdProduct, HttpStatus.CREATED);
+    @GetMapping("/api/products/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> requestedProduct = productService.getProductById(id);
+        if (requestedProduct.isPresent()) {
+            return new ResponseEntity<>(requestedProduct.get(), HttpStatus.OK);
+        } else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/api/products/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product updatedProduct) {
+        boolean wasUpdated = productService.updateProduct(id, updatedProduct);
+        if (!wasUpdated) {
+            return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 
--- src/main/java/com/femcoders/monster_shop/services/ProductService.java
+++ src/main/java/com/femcoders/monster_shop/services/ProductService.java
@@ -5,6 +5,7 @@ import com.femcoders.monster_shop.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -14,12 +15,37 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    public Product addProduct(Product newProduct) {
+        return productRepository.save(newProduct);
+    }
+
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
-    public Product addProduct(Product newProduct) {
-        return productRepository.save(newProduct);
+    public Optional<Product> getProductById(Long id) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        return optionalProduct;
     }
+
+    public boolean updateProduct (Long id, Product productToUpdate) {
+        Optional<Product> productDetails= productRepository.findById(id);
+        if (productDetails.isPresent()) {
+            Product newProductDetails = productDetails.get();
+            newProductDetails.setName(productToUpdate.getName());
+            newProductDetails.setPrice(productToUpdate.getPrice());
+            newProductDetails.setImageUrl(productToUpdate.getImageUrl());
+            newProductDetails.setRating(productToUpdate.getRating());
+            newProductDetails.setReviewCount(productToUpdate.getReviewCount());
+            newProductDetails.setFeatured(productToUpdate.isFeatured());
+            productRepository.save(newProductDetails);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
 
 }
